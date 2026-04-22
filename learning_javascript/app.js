b22ff66d7cd0e3b56b29ba5a85ebf338a335f6e3
Surya-ux-max <surprakas14@gmail.com>
2026-04-22 17:54:22 +0530

    Add variable declarations for name, age, and role in app.js

--- learning_javascript/app.js
+++ learning_javascript/app.js
@@ -1,1 +1,14 @@
+// console.log("Hello Developer! Welcome to learning JavaScript.");
+let name = "Suri";
+let age = 25;
+const Role = "AI Engineer";
+
 console.log("Hello Developer! Welcome to learning JavaScript.");
+console.log(name);
+console.log(age);
+console.log(Role);
+
+
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Role: " + Role);
