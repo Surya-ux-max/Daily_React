0f6be61e9b5f22470b3c240c6951698317d766dd
Surya-ux-max <surprakas14@gmail.com>
2026-04-23 21:51:59 +0530

    Comment out unused variable declarations and console logs in app.js

--- learning_javascript/app.js
+++ learning_javascript/app.js
@@ -1,34 +1,41 @@
+// // console.log("Hello Developer! Welcome to learning JavaScript.");
+// let name = "Suri";
+// let age = 25;
+// const Role = "AI Engineer";
+
 // console.log("Hello Developer! Welcome to learning JavaScript.");
-let name = "Suri";
-let age = 25;
-const Role = "AI Engineer";
-
-console.log("Hello Developer! Welcome to learning JavaScript.");
-console.log(typeof name);       // typeof in javascript will give the data type of the variable used in the console log
-console.log(typeof age);        // console.log will display the message the user is entering
-console.log(typeof Role);
+// console.log(typeof name);       // typeof in javascript will give the data type of the variable used in the console log
+// console.log(typeof age);        // console.log will display the message the user is entering
+// console.log(typeof Role);
 
 
-console.log("Name: " + name);
-console.log("Age: " + age);
-console.log("Role: " + Role);
+// console.log("Name: " + name);
+// console.log("Age: " + age);
+// console.log("Role: " + Role);
 
-console.log(10+10); // this will give us the sum of 10 and 10 which is 20
-console.log(10-5);
-console.log(10*5);
-console.log(10/2);
-console.log(10%3); // this will give us the remainder of 10 divided by 3 which is 1
+// console.log(10+10); // this will give us the sum of 10 and 10 which is 20
+// console.log(10-5);
+// console.log(10*5);
+// console.log(10/2);
+// console.log(10%3); // this will give us the remainder of 10 divided by 3 which is 1
 
-let ageofsurya = 20;
-ageofsurya -= 1;
-if(ageofsurya > 18){
-    console.log("Surya is an adult.");
+// let ageofsurya = 20;
+// ageofsurya -= 1;
+// if(ageofsurya > 18){
+//     console.log("Surya is an adult.");
+// }
+// else{
+//     console.log("Surya is a minor.");
+// }
+
+// let number = 10;
+// for( let i = 1;i<=number;i++){
+//     console.log("number : ",i);
+// }
+
+let myname = "Suri";
+greet(myname);
+function greet(myname) {
+    console.log("Hello " + myname + "! Welcome to learning JavaScript.");
 }
-else{
-    console.log("Surya is a minor.");
-}
 
-let number = 10;
-for( let i = 1;i<=number;i++){
-    console.log("number : ",i);
-}
